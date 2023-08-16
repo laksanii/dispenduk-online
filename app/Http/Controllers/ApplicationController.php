73dd58259bd2fa5809d76139030979da07a59b16
laksanii <prazetyo111@gmail.com>
2023-08-16 10:34:16 +0700

    feat: Create store application function

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Application;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class ApplicationController extends Controller
 {
-    //
+    public function storeApplication(Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required',
+            'service_type_id' => 'required',
+            'requirements' => 'required',
+            'requirements.*' => 'required|mimes:png,jpg,pdf,jpeg'
+        ]);
+
+        if($validator->fails()){
+            return $validator->errors();
+        }
+        
+        $requirements = [];
+        foreach($request->file('requirements') as $key => $value){
+            $requirements[$key] = $value->store('requirements/'.$key);
+        }
+
+        $newKK_application = Application::Create(array_merge(
+            $request->toArray(),
+            ['requirements'=> json_encode($requirements)]
+        ));
+
+        return $newKK_application;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApplicationController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -20,12 +21,18 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/menu', function () {
+    return view('dispenduk.layouts.app');
+});
+
+Route::post('/pengajuan-layanan', [ApplicationController::class, 'storeApplication']);
 
 require __DIR__.'/auth.php';
